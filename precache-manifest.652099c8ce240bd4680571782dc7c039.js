a957893dab81978444dff788e8424e2e1f95ff8a
Manish <manishzanzad123@gmail.com>
2019-04-22 19:34:56 +0530

    Updates

---
+++ precache-manifest.652099c8ce240bd4680571782dc7c039.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "04f1b92fda2f1eda8fc36b46d92da68e",
+    "url": "/SnakeGame/static/media/rat.04f1b92f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SnakeGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7f48931fc97ebe4e94f2",
+    "url": "/SnakeGame/static/js/runtime~main.86c35b2d.js"
+  },
+  {
+    "revision": "1db5dab92b98292f8028",
+    "url": "/SnakeGame/static/js/main.5bac88ed.chunk.js"
+  },
+  {
+    "revision": "cb8dee7671ca33191b16",
+    "url": "/SnakeGame/static/js/2.d372c466.chunk.js"
+  },
+  {
+    "revision": "1db5dab92b98292f8028",
+    "url": "/SnakeGame/static/css/main.eee2828e.chunk.css"
+  },
+  {
+    "revision": "cb8dee7671ca33191b16",
+    "url": "/SnakeGame/static/css/2.71af19fe.chunk.css"
+  },
+  {
+    "revision": "dcbcea80bf4bc1bdf2d2178a99a74334",
+    "url": "/SnakeGame/index.html"
+  }
+];
